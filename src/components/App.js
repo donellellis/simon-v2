a4129317c586b1135a197a364fdf364688dd9cfd
DONELL ELLIS <DONELL.ELLIS@GMAIL.COM>
2019-03-24 13:44:58 -0400

    computer generated sequence working

--- src/components/App.js
+++ src/components/App.js
@@ -2,21 +2,71 @@ import React, { Component } from 'react'
 import Button from './Button'
 
 class App extends Component {
-  
+  constructor(props){
+    super(props)
+    this.state = {
+      sequence: [],
+      selectedNumber: null
+    }
+  }
+
+  setDelay = () => {
+    for (let i = 0; i < this.state.sequence.length; i++){
+      //sets time interval for button illumination
+      setTimeout(() => this.setState({
+        selectedNumber: this.state.sequence[i]
+      }), (500 + (1000 * i)))
+
+      //sets time interval for delay between button illumination
+      setTimeout(() => this.setState({
+        selectedNumber: null
+      }), 1000 + (1000 * i) )
+    }
+  }
+
+ 
+  playSequence = () => {
+    // selects 4 random numbers between 0 and 4 and adds to sequence
+    let sequenceLevelOne = []
+    for (let i = 0; i < 4; i++){
+      sequenceLevelOne.push(Math.floor((Math.random() * 4) + 0))
+    }
+    this.setState({
+      sequence: sequenceLevelOne
+    }, () => this.setDelay())
+  }
+
   render() {
+
     return (
       <div className='app'>
         <header>
           <h1>Simon</h1>
         </header>
         <main>
-          <Button className={'buttonTopLeft'}/>
-          <Button className={'buttonTopRight'}/>
-          <Button className={'buttonBottomLeft'}/>
-          <Button className={'buttonBottomRight'}/>
+          <Button 
+            id={0}
+            selectedNumber={this.state.selectedNumber}
+            className={'buttonTopLeft'}
+          />
+          <Button
+            id={1}
+            selectedNumber={this.state.selectedNumber}
+            className={'buttonTopRight'}
+          />
+          <Button
+            id={2}
+            selectedNumber={this.state.selectedNumber}
+            className={'buttonBottomLeft'}
+          />
+          <Button
+            id={3}
+            selectedNumber={this.state.selectedNumber}
+            className={'buttonBottomRight'}
+          />
         </main>
         <nav>
-          <button className='buttonNav fas fa-play'></button>
+          <button className='buttonNav fas fa-play' onClick={this.playSequence}/>
         </nav>
       </div>
     );
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,36 +1,50 @@
 import React, { Component } from 'react'
 
 class Button extends Component{
-    constructor(){
-        super()
-        this.state = {
-            isIlluminated: false
-        }
-        this.receiveUserClick = this.receiveUserClick.bind(this)
-        this.toggleIllumination = this.toggleIllumination.bind(this)
-        this.delayToggle = this.delayToggle.bind(this)
-    }
-    
-    receiveUserClick(){
-        this.toggleIllumination()
-        this.delayToggle()
-    }
+    // constructor(props){
+    //     super(props)
+    //     this.state = {
+    //         isIlluminated: false,
+    //         isSelected: false
+    //     }
+    //     // this.handleUserClick = this.handleUserClick.bind(this)
+    //     // this.toggleIllumination = this.toggleIllumination.bind(this)
+    //     // this.delayToggle = this.delayToggle.bind(this)
+    // }
 
-    toggleIllumination(){
-        this.setState({
-            isIlluminated: !this.state.isIlluminated
-        })
-        
-    }
+    // componentDidUpdate(){
+    //     if(this.props.id === this.props.slectedNumber){
+    //         console.log('this is the number ' + this.props.slectedNumber)
+    //     }
+    //     else{
+    //         console.log('updated ' + this.props.slectedNumber )
+    //     }
+    // }
+ 
+    // handleUserClick(){
+    //     this.toggleIllumination()
+    //     this.delayToggle()
+    // }
 
-    delayToggle(){
-        setTimeout(this.toggleIllumination, 500)
-    }
+    // toggleIllumination(){
+    //     this.setState({
+    //         isIlluminated: !this.state.isIlluminated
+    //     })
+    // }
 
+    // delayToggle(){
+    //     setTimeout(this.toggleIllumination, 500)
+    // }
 
     render(){
+        let isIlluminated = false
+        if (this.props.id === this.props.selectedNumber){
+            isIlluminated = true
+        }
+        // const isIlluminated = this.props.id === this.props.slectedNumber
+
         return(
-            <div className={this.state.isIlluminated ? this.props.className + 'Illuminated': this.props.className} onClick={this.receiveUserClick}>
+            <div className={isIlluminated ? this.props.className + 'Illuminated': this.props.className} onClick={this.handleUserClick}>
             </div>
         )
     }
